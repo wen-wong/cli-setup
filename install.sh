3f53f2f667ac0064140535ebeff375e92352abe1
wen-wong <wen.wong@mail.mcgill.ca>
2024-07-23 20:27:21 -0400

    feat: Add neovim installation option

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Code Reference: https://github.com/bartekspitza/dotfiles/blob/master/install.sh
+function ask() {
+	read -p "$1 (Y/n): " resp
+	if [ -z "$resp" ]; then
+		response_lc="y"
+	else
+		response_lc=$(echo "$resp" | tr '[:upper:]' '[:lower:]')
+	fi
+
+	[ "$response_lc" = "y" ]
+}
+
+if ask "Do you want to install neovim?"; then
+	sudo apt install neovim
+fi
