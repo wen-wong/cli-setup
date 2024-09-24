0aefaccb6186b2ae7007afe1fde443d5daa0aff8
wen-wong <wen.wong@mail.mcgill.ca>
2024-09-24 15:17:51 -0400

    fix: Add user execution permission

--- install/package/neovim-setup.sh
+++ install/package/neovim-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen ripgrep
-git clone https://github.com/neovim/neovim.git /opt/
-cd /opt/
+git clone https://github.com/neovim/neovim.git 
+cd $HOME/neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 cd $HOME
